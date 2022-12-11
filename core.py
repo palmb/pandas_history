62d3464b10b10e1517b71874a38b1de816a177d4
Bert Palm <bert.palm@ufz.de>
2022-12-11 03:24:59 +0100

    works like a charm

--- core.py
+++ core.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import logging
 from functools import reduce
 
 import numpy as np
@@ -26,8 +27,8 @@ class FullSeriesHistory:
         we store the entire new series
     """
 
-    BASE_FAKTOR = 0.67
-    __change_keys = {"add", "rm", "mod"}
+    BASE_FAKTOR = 0.5
+    __change_keys = {"add", "rm", "mod", "dtype"}
 
     def __init__(self, s: pd.Series | None = None, copy=True):
         self._bases = []
@@ -53,79 +54,67 @@ class FullSeriesHistory:
         """may raise IndexError for index out of range."""
         return range(len(self) if sz is None else sz)[int(i)]
 
-    def update(self, series: pd.Series, skipna=False,  copy=True):
+    def update(self, series: pd.Series, copy=True):
         """
         skipna: bool
+            Ignore nan's
             Only update valid values. In other words,
             keep the previous value if the given series is NaN.
         """
         assert isinstance(series, pd.Series)
         s = series.copy(deep=copy)
+        logging.debug(f"len: {len(s)}")
 
-        # differences in index
-        add = s.reindex(s.index.difference(self._current.index))
         rm = self._current.index.difference(s.index)
-        # differences in values
-        mod = self._get_modified(s, skipna)
+        add = s.reindex(s.index.difference(self._current.index))
+        mod = self._get_modified(s)
 
-        # optimisation
-        if (
-            (not skipna or not s.hasnans)
-            and len(mod) + len(add) > self.BASE_FAKTOR * len(self._current)
-        ):
+        logging.debug(
+            f"rm: {len(rm)}, add: {len(add)}, mod: {len(mod)} => {len(mod) + len(add)} >? {self.BASE_FAKTOR * (len(self._current) - len(rm))}")
+
+        if len(mod) + len(add) > self.BASE_FAKTOR * (len(self._current) - len(rm)):
             return self._set_new_base(s)
-        if add.empty:
-            add = None
-        if rm.empty:
-            rm = None
-        if mod.empty:
-            mod = None
-        if add is None and rm is None and mod is None:
+
+        to_update = dict()
+        if not rm.empty:
+            to_update['rm'] = rm
+        if not add.empty:
+            to_update['add'] = add
+        if not mod.empty:
+            to_update['mod'] = mod
+        if s.dtype != self._current.dtype:
+            to_update['dtype'] = s.dtype
+
+        assert s.equals(self._apply(self._current.copy(), to_update))
+
+        if not to_update:
             return self._update(None)
-        
-        return self._update(dict(add=add, rm=rm, mod=mod))
 
-    def _get_modified(self, s, skipna):
-        if skipna or not s.hasnans:
-            common = s.dropna().index.intersection(self._current.index)
-            s, c = s.reindex(common), self._current.reindex(common)
-            return s.loc[~(s == c)]
+        return self._update(to_update)
 
-        # 1. calculate rows where either the new
-        #    series xor the current is NaN.
-        # 2. calculate rows where the non-NaN values differ
+    def _get_modified(self, s):
         common = s.index.intersection(self._current.index)
         s, c = s.reindex(common), self._current.reindex(common)
-        return s.loc[(s.isna() ^ c.isna()) | ~(s == c)]
+        both_nan = c.isna() & s.isna()
+        value_change = (c != s) & ~both_nan
+        identical = (c == s) | both_nan
+        logging.debug(f"identical: {len(identical[identical])}")
+        return s.loc[value_change]
 
     def _update(self, base_or_change):
         """insert base or change-dict and set '_current'."""
         if isinstance(base_or_change, pd.Series):  # a new base
-            return self._update_with_base(base_or_change)
-        return self._update_with_change(base_or_change)
+            self._bases.append(base_or_change)
+            self._changes.append(None)
+            self._current = self._bases[-1]
+        else:
+            if base_or_change is not None:
+                assert isinstance(base_or_change, dict)
+                self._current = self._apply(self._current.copy(), base_or_change)
+            self._changes.append(base_or_change)
+            self._bases.append(self._get_base_index())
 
     _set_new_base = _update  # alias
-
-    def _update_with_base(self, base):
-        self._bases.append(base)
-        self._changes.append(None)
-        self._current = self._bases[-1]
-
-    def _update_with_change(self, change):
-        if change is None:
-            pass
-        elif isinstance(change, dict):
-            assert self.__change_keys == change.keys()
-            if all(map(lambda x: x is None, change.values())):
-                change = None
-        else:
-            raise TypeError(type(change))
-        if change is not None:
-            # first call self._apply to ensure the data
-            # is valid, before storing it
-            self._current = self._apply(self._current.copy(), change)
-        self._changes.append(change)
-        self._bases.append(self._get_base_index())
 
     def get(self):
         return self.restore(i=None)
@@ -141,7 +130,7 @@ class FullSeriesHistory:
         base = self._bases[base_i]
         assert isinstance(base, pd.Series)
         result = base.copy()
-        changes = [d for d in self._changes[base_i + 1 : i + 1] if d is not None]
+        changes = [d for d in self._changes[base_i + 1: i + 1] if d is not None]
         for change in changes:
             assert isinstance(change, dict)
             result = self._apply(result, change)
@@ -149,15 +138,21 @@ class FullSeriesHistory:
 
     @staticmethod
     def _apply(base: pd.Series, change):
-        add: pd.Series = change["add"]
-        rm: pd.Index = change["rm"]
-        mod: pd.Series = change["mod"]
+        rm: pd.Index = change.get('rm')
+        add: pd.Series = change.get("add")
+        mod: pd.Series = change.get('mod')
+        dtype = change.get('dtype')
+        if rm is not None:
+            base = base.drop(rm)
+        if dtype is not None:
+            base = base.astype(dtype)
         if add is not None:
             base = base.reindex(base.index.union(add.index))
             base.loc[add.index] = add
         if mod is not None:
             base.loc[mod.index] = mod
-        if rm is not None:
+        # works with None, ensure inserts did not change dtype
+        if dtype is not None:
             base = base.drop(rm)
         return base
 
@@ -170,10 +165,10 @@ class FullSeriesHistory:
         df = df.astype(str)
         for i in df.columns:
             idx = df.index.difference(serieses[i].index)
-            df.loc[idx, i] = ''
+            df.loc[idx, i] = ""
 
         bases = self._bases
-        columns = [i if isinstance(bases[i], int) else f'{i}(base)' for i in df.columns]
+        columns = [i if isinstance(bases[i], int) else f"{i}(base)" for i in df.columns]
         df.columns = columns
 
         print(df)
@@ -182,17 +177,79 @@ class FullSeriesHistory:
         return str(self._current)
 
 
-def test_empty():
-    h = FullSeriesHistory()
-    assert h.empty
-    h.update(pd.Series([], dtype=float))
-    assert not h.empty
-
-
 def get_size(obj):
     import pickle
 
     return len(pickle.dumps(obj))
+
+
+def foo():
+    i0 = pd.date_range("2000", None, 20)
+    s0 = pd.Series(np.nan, index=i0, dtype=float)
+    h = FullSeriesHistory(s0)
+    print(get_size(h))
+
+    # flag all stuff, change dtype
+    s = s0.copy()
+    s.iloc[:] = "foo"
+    h.update(s)
+    print(get_size(h))
+
+    # flag stuff
+    curr = h.get()
+    curr.iloc[::2] = 10
+    h.update(curr)
+    print(get_size(h))
+
+    # flag same stuff
+    s = s0.copy()
+    s.iloc[5::2] = 10
+    h.update(s)
+    print(get_size(h))
+
+    # reindex stuff
+    i1 = pd.date_range("2000-01-10", None, 15)
+    curr = h.get().reindex(i1)
+    h.update(curr)
+    print(get_size(h))
+
+    # flag and reindex stuff
+    curr = h.get().shift(freq="-1d")
+    curr.iloc[7:10] = 25.3
+    h.update(curr)
+    print(get_size(h))
+
+    # flag nothing
+    curr = h.get()
+    curr.iloc[:] = np.nan
+    h.update(curr)
+    print(get_size(h))
+
+    # flag all
+    curr = h.get()
+    curr.iloc[:] = 99
+    h.update(curr)
+    print(get_size(h))
+
+    h.pprint()
+
+
+def identical_test():
+    i0 = pd.date_range("2000", None, 20)
+    s0 = pd.Series(np.nan, index=i0, dtype=float)
+    h = FullSeriesHistory(s0)
+    s1 = s0.copy()
+    s1.iloc[:4] = 99
+    h.update(s1)
+    h.update(s1)
+    s2 = s1.copy()
+    s2 = s2.iloc[:4]
+    h.update(s2)
+    h.pprint()
+    assert h.restore(0).equals(s0)
+    assert h.restore(1).equals(s1)
+    assert h.restore(2).equals(s1)
+    assert h.restore(3).equals(s2)
 
 
 if __name__ == "__main__":
@@ -200,39 +257,6 @@ if __name__ == "__main__":
 
     warnings.filterwarnings("ignore", ".*iteritems")
     warnings.filterwarnings("ignore", ".*with an integer-dtype index is deprecated")
-    i0 = pd.date_range('2000', None, 20)
-    s0 = pd.Series(np.nan, index=i0, dtype=float)
-    h = FullSeriesHistory(s0)
-    print(get_size(h))
-
-    # flag stuff
-    curr = h.get()
-    curr.iloc[::2] = 10
-    h.update(curr, skipna=True)
-    print(get_size(h))
-
-    # reindex stuff
-    i1 = pd.date_range('2000-01-10', None, 15)
-    curr = h.get().reindex(i1)
-    h.update(curr, skipna=True)
-    print(get_size(h))
-
-    # flag and reindex stuff
-    curr = h.get().shift(freq='-1d')
-    curr.iloc[7:10] = 25.3
-    h.update(curr, skipna=False)
-    print(get_size(h))
-
-    # flag nothing
-    curr = h.get()
-    curr.iloc[:] = np.nan
-    h.update(curr, skipna=True)
-    print(get_size(h))
-
-    # flag all
-    curr = h.get()
-    curr.iloc[:] = 99
-    h.update(curr, skipna=True)
-    print(get_size(h))
-
-    h.pprint()
+    logging.basicConfig(level="DEBUG")
+    # identical_test()
+    foo()
